bfb9d938eafb4a4ac2ec562d6960f0a80d0ed2aa
spoonily <jonahm2472@gmail.com>
2016-02-22 14:09:09 -0500

    Javascript Works!
    I made a few silly mistakes, but now the routing works as intended! Now
    to create the views.

--- Controllers/homescript.js
+++ Controllers/homescript.js
@@ -1,28 +1,52 @@
-var homeApp = angular.module('homeApplication', ['ngRoute']);
+var HomeApp = angular.module('HomeApp', ['ngRoute']);
 
-homeApp.controller('HomeController', function($scope) {
-	$scope.message = "hi";
-});
-
-homeApp.config(function($routeProvider) {
+HomeApp.config(function($routeProvider) {
 
 	$routeProvider
 
-		.when('/homepage.html', {
-			templateUrl : 'about.html',
+		.when('/', {
+			templateUrl : 'pages/title.html',
+			controller : 'TitleController'
 		})
 
-		.when('/Contact' {
-			templateUrl : 'contact.html',
+		.when('/Projects', {
+			templateUrl : 'Pages/projects.html',
+			controller : 'ProjectController'
 		})
 
-		.when('/Projects' {
-			templateUrl : 'projects.html',
-
+		.when('/About', {
+			templateUrl : 'Pages/about.html'
+			controller : 'AboutController'
 		})
 
-		.when('/Resume' {
-			templateUrl : 'resume.html',
+		.when('/Resume', {
+			templateUrl : 'Pages/resume.html',
+			controller : 'ResumeController'
+		})
+
+		.when('/Contact', {
+			templateUrl : 'Pages/contact.html',
+			controller : 'ContactController'
 		});
 
 });
+
+HomeApp.controller('TitleController', function($scope) {
+	$scope.title = "Welcome to My Personal Website!";
+})
+
+HomeApp.controller('ProjectController', function($scope) {
+	$scope.title = "Here are some Projects I've Made!";
+})
+
+HomeApp.controller('AboutController', function($scope) {
+	$scope.title = "Nice to Meet You!";
+})
+
+HomeApp.controller('ResumeController', function($scope) {
+	$scope.title = "Click the Link for a PDF of my Resume!";
+})
+
+HomeApp.controller('ContactController', function($scope) {
+	$scope.title = "Here is my Contact Information!";
+})
